273baf8ff6b63ae979b50f07151b9ddf2db755a0
Brblal <124357245+Brblal@users.noreply.github.com>
2024-01-18 12:55:54 +0100

    Update api.py

--- website/api.py
+++ website/api.py
@@ -14,6 +14,7 @@ api_rest = Api(api)
 parser = reqparse.RequestParser()
 parser.add_argument('title_before', type=str)
 parser.add_argument('first_name', type=str)
+parser.add_argument('t_uuid', type=uuid.UUID)
 # Přidejte další argumenty podle potřeby
 
 def listToString(s):
@@ -31,13 +32,13 @@ class LecturerResource(Resource):
     def get(self, uuid=None):
         if uuid:
             # Handle GET request for a specific lecturer
-            teacher = Teacher.query.filter_by(UUID=uuid).first()
+            teacher = Teacher.query.filter_by(t_uuid=uuid).first()
             if teacher:
                 contact = Contact.query.filter_by(teacher_id=uuid).first()
                 tags = Tag.query.filter_by(teacher_id=uuid).all()
 
                 lecturer_data = {
-                    'UUID': teacher.UUID,
+                    'UUID': teacher.t_uuid,
                     'title_before': teacher.title_before,
                     'first_name': teacher.first_name,
                     'middle_name': teacher.middle_name,
@@ -62,11 +63,11 @@ class LecturerResource(Resource):
             lecturer_list = []
 
             for teacher in teachers:
-                contact = Contact.query.filter_by(teacher_id=teacher.UUID).first()
-                tags = Tag.query.filter_by(teacher_id=teacher.UUID).all()
+                contact = Contact.query.filter_by(teacher_id=teacher.t_uuid).first()
+                tags = Tag.query.filter_by(teacher_id=teacher.t_uuid).all()
 
                 lecturer_data = {
-                    'UUID': teacher.UUID,
+                    'UUID': teacher.t_uuid,
                     'title_before': teacher.title_before,
                     'first_name': teacher.first_name,
                     'middle_name': teacher.middle_name,
@@ -89,48 +90,41 @@ class LecturerResource(Resource):
 
 
     def post(self):
-        
+        parser = reqparse.RequestParser()
+        parser.add_argument('t_uuid', type=str, required=True)
+        parser.add_argument('first_name', type=str, required=True)
+        parser.add_argument('last_name', type=str, required=True)
+        parser.add_argument('middle_name', type=str, required=True)
+        parser.add_argument('title_before', type=str, required=True)
+        parser.add_argument('title_after', type=str, required=True)
+        parser.add_argument('picture_url', type=str, required=True)
+        parser.add_argument('location', type=str, required=True)
+        parser.add_argument('claim', type=str, required=True)
+        parser.add_argument('bio', type=str, required=True)
+        parser.add_argument('price_per_hour', type=int, required=True)
+        parser.add_argument('telephone_numbers', type=str, required=True)
+        parser.add_argument('emails', type=str, required=True)
+        parser.add_argument('tags', [])
+
         args = parser.parse_args()
-        provided_uuid = args.get("UUID")
-        if not provided_uuid:
-            # If UUID is not provided, generate a new one
-            provided_uuid = generate_uuid()
-        new_teacher = Teacher(
-            UUID=provided_uuid,
-            title_before=args.get("title_before"),
-            first_name=args.get("first_name"),
-            middle_name=args.get("middle_name"),
-            last_name=args.get("last_name"),
-            title_after=args.get("title_after"),
-            picture_url=args.get("picture_url"),
-            location=args.get("location"),
-            claim=args.get("claim"),
-            bio=args.get("bio"),
-            price_per_hour=args.get("price_per_hour")
-        )
-        db.session.add(new_teacher)
-        db.session.commit()
 
+        lecturer = Teacher(title_before = args['title_before'], first_name=args['first_name'], middle_name = args['middle_name'] , last_name=args['last_name'], title_after = args['title_after'] , picture_url = args['picture_url'], location = args['location'] , claim = args['claim'] , bio = args['bio'] , price_per_hour = args['price_per_hour'] , t_uuid=args['t_uuid'])
+        db.session.add(lecturer)
+        db.session.commit()
         tags = [Tag(uuid=tag["uuid"], name=tag["name"]) for tag in args.get("tags", [])]
-        new_teacher.tags = tags
-        
-        
-
-    
-       
-        telephone_numbers = listToString(args.get("contact", {}).get("telephone_numbers", []))
-
-        emails=listToString(args.get("contact", {}).get("emails", []))
-        
-        new_contact = Contact(telephone_numbers=telephone_numbers, emails=emails, teacher_id= new_teacher.UUID)
-        db.session.add(new_contact)
+        lecturer.tags = tags
+        telephone_numbers = args['telephone_numbers']
+        emails = args['emails']
+        contact = Contact(telephone_numbers = telephone_numbers, emails = emails, teacher_id = lecturer.t_uuid)
+        db.session.add(contact)
         db.session.commit()
-
-        return {'message': 'Lecturer created successfully'}, 201
+        
+            
+        return jsonify({"title_before": lecturer.title_before, "first_name": lecturer.first_name, "middle_name": lecturer.middle_name, "last_name": lecturer.last_name, "title_after": lecturer.title_after, "pictue_url": lecturer.picture_url, "location": lecturer.location, "claim": lecturer.claim, "bio": lecturer.bio, "price_per_hour": lecturer.price_per_hour,"tags": lecturer.tags, "contact":{"telephone_numers": contact.telephone_numbers, "emails": contact.emails}, "UUID": lecturer.t_uuid})
 
     def put(self, uuid):
         args = parser.parse_args()
-        teacher = Teacher.query.filter_by(UUID=uuid).first()
+        teacher = Teacher.query.filter_by(t_uuid=uuid).first()
 
         if teacher:
             teacher.title_before = args['title_before']
@@ -145,7 +139,7 @@ class LecturerResource(Resource):
     def delete(self, uuid=None):
         if uuid:
             # Handle DELETE request for a specific lecturer
-            teacher = Teacher.query.filter_by(UUID=uuid).first()
+            teacher = Teacher.query.filter_by(t_uuid=uuid).first()
 
             if teacher:
                 db.session.delete(teacher)
@@ -166,4 +160,5 @@ class LecturerResource(Resource):
             
             return {'message': 'All lecturers deleted successfully'}, 200
 
-api_rest.add_resource(LecturerResource, '/lecturer', '/lecturer/<uuid>')
+api_rest.add_resource(LecturerResource, '/lecturers', '/lecturers/<uuid>')
+
